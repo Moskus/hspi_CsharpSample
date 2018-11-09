0de38fe9a918c6a82c870f2dfbbb2e33e9cde048
Odd Erik Midthaug <oesolberg@hotmail.com>
2018-11-09 18:43:13 +0100

    Connecting

---
+++ hspi_CsharpSample/Hspi.cs
@@ -0,0 +1,277 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using HomeSeerAPI;
+
+namespace hspi_CsharpSample
+{
+	//IPlugInAPI - this API is required for ALL plugins
+	//IThermostatAPI   ' add this API if this plugin supports thermostats
+	public class Hspi: IPlugInAPI
+	{
+		public int Capabilities()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public int AccessLevel()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool SupportsMultipleInstances()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool SupportsMultipleInstancesSingleEXE()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool SupportsAddDevice()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public string InstanceFriendlyName()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public IPlugInAPI.strInterfaceStatus InterfaceStatus()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void HSEvent(Enums.HSEvent EventType, object[] parms)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public string GenPage(string link)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public string PagePut(string data)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void ShutdownIO()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool RaisesGenericCallbacks()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void SetIOMulti(List<CAPI.CAPIControl> colSend)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public string InitIO(string port)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public IPlugInAPI.PollResultInfo PollDevice(int dvref)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool SupportsConfigDevice()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool SupportsConfigDeviceAll()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Enums.ConfigDevicePostReturn ConfigDevicePost(int @ref, string data, string user, int userRights)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public string ConfigDevice(int @ref, string user, int userRights, bool newDevice)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public SearchReturn[] Search(string SearchString, bool RegEx)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <summary>
+		/// This will allow scripts to call any function in your plugin by name.
+		/// Used to allow scripting of your plugin functions.
+		/// </summary>
+		/// <param name="proc">The name of the process/sub/function to call</param>
+		/// <param name="parms">Parameters passed as an object array</param>
+		/// <returns></returns>
+		/// <remarks>http://homeseer.com/support/homeseer/HS3/SDK/custom_functions.htm</remarks>
+		public object PluginFunction(string procName, object[] parms)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <summary>
+		/// This will allow scripts to call any property in your plugin by name.
+		/// Used to allow scripting of your plugin properties.
+		/// </summary>
+		/// <param name="proc">The name of the Property to get</param>
+		/// <param name="parms">Parameters passed as an object array</param>
+		/// <returns></returns>
+		/// <remarks>http://homeseer.com/support/homeseer/HS3/SDK/custom_functions.htm</remarks>
+		public object PluginPropertyGet(string procName, object[] parms)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		
+		/// <summary>
+		/// This will allow scripts to set any property in your plugin by name.
+		/// Used to allow scripting of your plugin properties.
+		/// </summary>
+		/// <param name="proc">The name of the property to set</param>
+		/// <param name="value">The new value for the property</param>
+		/// <remarks>http://homeseer.com/support/homeseer/HS3/SDK/custom_functions.htm</remarks>
+		public void PluginPropertySet(string procName, object value)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void SpeakIn(int device, string txt, bool w, string host)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public int ActionCount()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool ActionConfigured(IPlugInAPI.strTrigActInfo ActInfo)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public string ActionBuildUI(string sUnique, IPlugInAPI.strTrigActInfo ActInfo)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public IPlugInAPI.strMultiReturn ActionProcessPostUI(NameValueCollection PostData, IPlugInAPI.strTrigActInfo TrigInfoIN)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public string ActionFormatUI(IPlugInAPI.strTrigActInfo ActInfo)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool ActionReferencesDevice(IPlugInAPI.strTrigActInfo ActInfo, int dvRef)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool HandleAction(IPlugInAPI.strTrigActInfo ActInfo)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public string TriggerBuildUI(string sUnique, IPlugInAPI.strTrigActInfo TrigInfo)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public IPlugInAPI.strMultiReturn TriggerProcessPostUI(NameValueCollection PostData, IPlugInAPI.strTrigActInfo TrigInfoIN)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public string TriggerFormatUI(IPlugInAPI.strTrigActInfo TrigInfo)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool TriggerTrue(IPlugInAPI.strTrigActInfo TrigInfo)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool TriggerReferencesDevice(IPlugInAPI.strTrigActInfo TrigInfo, int dvRef)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public string GetPagePlugin(string page, string user, int userRights, string queryString)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public string PostBackProc(string page, string data, string user, int userRights)
+		{
+			throw new System.NotImplementedException();
+		}
+		/// <summary>
+		/// Returns the name of your plug-in. This is used to identify your plug-in to HomeSeer and your users. Keep the name to 16 characters or less.
+		/// Do not access any hardware in this function as HomeSeer will call this function.
+		/// Do NOT use special characters in your plug-in name with the exception of "-", ".", and " " (space).
+		/// </summary>
+		/// <returns>Plugin name</returns>
+		/// <remarks>http://homeseer.com/support/homeseer/HS3/SDK/name.htm</remarks>
+		public string Name { get; }
+
+		public bool HSCOMPort { get; }
+		public bool ActionAdvancedMode { get; set; }
+		public string get_ActionName(int ActionNumber)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool get_HasConditions(int TriggerNumber)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool HasTriggers { get; }
+		public int TriggerCount { get; }
+		public string get_TriggerName(int TriggerNumber)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public int get_SubTriggerCount(int TriggerNumber)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public string get_SubTriggerName(int TriggerNumber, int SubTriggerNumber)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool get_TriggerConfigured(IPlugInAPI.strTrigActInfo TrigInfo)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool get_Condition(IPlugInAPI.strTrigActInfo TrigInfo)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void set_Condition(IPlugInAPI.strTrigActInfo TrigInfo, bool Value)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ hspi_CsharpSample/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using HomeSeerAPI;
+using HSCF.Communication.Scs.Communication;
+using HSCF.Communication.Scs.Communication.EndPoints.Tcp;
+using HSCF.Communication.ScsServices.Client;
+
+namespace hspi_CsharpSample
+{
+	class Program
+	{
+		private static string _instance;
+		private static int _port;
+		private static Hspi _appApi;
+		private static string _ip;
+		private static IHSApplication _host;
+		private static IAppCallbackAPI _callback;
+
+		static void Main(string[] args)
+		{
+			_ip = "127.0.0.1";//'"" 'Default is connecting to the local server
+			_port = 10400;//Default port
+
+			//Let's check the startup arguments.Here you can set the server location(IP) if you are running the plugin remotely, and set an optional instance name
+
+			foreach (var argument in args)
+			{
+				var parts = argument.Split('=');
+				switch (parts[0])
+				{
+					case "port":
+						_port = 10400;
+						int tempPort = 0;
+						if (int.TryParse(parts[1], out tempPort))
+						{
+							_port = tempPort;
+						}
+						break;
+					case "server":
+						_ip = parts[1];
+						break;
+					case "instance":
+						try
+						{
+							_instance = parts[1];
+						}
+						catch (Exception ex)
+						{
+							Console.WriteLine(ex.Message);
+							_instance = "";
+						}
+						break;
+				}
+			}
+
+			_appApi = new Hspi();
+			Console.WriteLine("Connecting to server at " + _ip + ":" + _port + "...");
+
+			_client = ScsServiceClientBuilder.CreateClient<IHSApplication>(new ScsTcpEndPoint(_ip, _port),
+					_appApi);
+			_callbackClient = ScsServiceClientBuilder.CreateClient<IAppCallbackAPI>(
+					new ScsTcpEndPoint(_ip, _port), _appApi);
+
+			var retryAttempts = 1;
+			var isConnected= false;
+			do
+			{
+				try
+				{
+					
+					_client.Connect();
+					_callbackClient.Connect();
+					_host = _client.ServiceProxy;
+					var apiVersion = _host.APIVersion;//will cause an error if not really connected
+					_callback = _callbackClient.ServiceProxy;
+					apiVersion = _callback.APIVersion;//will cause an error if not really connected
+					isConnected = true;
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("Cannot connect! Attempt " + retryAttempts.ToString() + ": " + ex.Message);
+
+					if (ex.Message.ToLower().Contains("timeout occurred."))
+					{
+						retryAttempts++;
+						SleepSeconds(4);
+					}
+
+					if (retryAttempts > 5 && isConnected==false)
+					{
+						_client?.Dispose();
+						_client = null;
+						_callbackClient?.Dispose();
+						_callbackClient = null;
+					}
+				}
+			} while (retryAttempts < 6 && isConnected == false);
+
+			try
+			{
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				throw;
+			}
+			//		appAPI = New hspi
+
+			//			Console.WriteLine("Connecting to server at " & ip & "...");
+
+			//		client = ScsServiceClientBuilder.CreateClient(Of IHSApplication)(New ScsTcpEndPoint(ip, 10400), appAPI)
+
+			//		clientCallback = ScsServiceClientBuilder.CreateClient(Of IAppCallbackAPI)(New ScsTcpEndPoint(ip, 10400), appAPI)
+
+
+			//		Dim Attempts As Integer = 1
+
+			//TryAgain:
+			//			Try
+
+			//			client.Connect()
+
+			//			clientCallback.Connect()
+
+
+			//			host = client.ServiceProxy
+
+			//			Dim APIVersion As Double = host.APIVersion  ' will cause an error if not really connected
+
+
+			//			callback = clientCallback.ServiceProxy
+
+			//			APIVersion = callback.APIVersion  ' will cause an error if not really connected
+
+			//		Catch ex As Exception
+
+			//			Console.WriteLine("Cannot connect attempt " & Attempts.ToString & ": " & ex.Message)
+
+			//			If ex.Message.ToLower.Contains("timeout occurred.") Then
+			//				Attempts += 1
+
+			//				If Attempts< 6 Then GoTo TryAgain
+			//		   End If
+
+			//		   If client IsNot Nothing Then
+
+			//				client.Dispose()
+
+			//				client = Nothing
+
+			//			End If
+
+			//			If clientCallback IsNot Nothing Then
+
+			//				clientCallback.Dispose()
+
+			//				clientCallback = Nothing
+
+			//			End If
+
+			//			SleepSeconds(4)
+
+			//			Return
+			//		End Try
+
+			//		Try
+
+			//			' create the user object that is the real plugin, accessed from the pluginAPI wrapper
+
+			//			callback = callback
+
+
+			//		hs = host
+
+			//			' connect to HS so it can register a callback to us
+
+			//			host.Connect(plugin.Name, "")
+
+			//			Console.WriteLine("Connected, waiting to be initialized...")
+
+			//			Do
+
+			//				Threading.Thread.Sleep(30)
+
+			//			Loop While client.CommunicationState = HSCF.Communication.Scs.Communication.CommunicationStates.Connected And Not IsShuttingDown
+			//			If Not IsShuttingDown Then
+
+			//				plugin.ShutdownIO()
+
+			//				Console.WriteLine("Connection lost, exiting")
+
+			//			Else
+
+			//				Console.WriteLine("Shutting down plugin")
+
+			//			End If
+
+			//			' disconnect from server for good here
+
+			//			client.Disconnect()
+
+			//			clientCallback.Disconnect()
+
+			//			SleepSeconds(2)
+
+			//			End
+			//		Catch ex As Exception
+
+			//			Console.WriteLine("Cannot connect(2): " & ex.Message)
+
+			//			SleepSeconds(2)
+
+			//			End
+			//			Return
+
+			//		End Try
+		}
+
+		private static void SleepSeconds(int secondsToSleep)
+		{
+			Thread.Sleep(secondsToSleep * 1000);
+		}
+
+		public static IScsServiceClient<IAppCallbackAPI> _callbackClient { get; set; }
+
+		public static IScsServiceClient<IHSApplication> _client { get; set; }
+	}
+}
